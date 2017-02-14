f58b8134d0209abaf6196e02a91272c6de4ab758
SandeshVakale <sandyrv.vakale@gmail.com>
2017-02-14 18:30:45 +0100

    Update AuthenticateUser.java

--- src/fr/epita/iam/authentication/AuthenticateUser.java
+++ src/fr/epita/iam/authentication/AuthenticateUser.java
@@ -3,9 +3,7 @@ package fr.epita.iam.authentication;
 public class AuthenticateUser {
 	public boolean authenticate(String username, String password) {
 		// authentication method
-		if ("adm".equals(username) && "pwd".equals(password))
-		return true;
-		else 
-			return false;
+		return ("adm".equals(username) && "pwd".equals(password));
+		
 	}
 }
